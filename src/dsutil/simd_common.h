b1efbbfa895d5e69e98d1a7ea8746ca298bcac8b
yateplofizik@yandex.ru <yateplofizik@yandex.ru@a4e608f0-1b1b-11df-b263-d5acc5e2fff2>
2010-05-17 21:07:51 +0000

    backported mpc hc changeset 1891 cleanup

--- src/dsutil/simd_common.h
+++ src/dsutil/simd_common.h
@@ -1,30 +1,36 @@
 #ifndef _SIMD_COMMON_H_
 #define _SIMD_COMMON_H_
 
-#ifdef __GNUC__
-#define __forceinline __attribute__((__always_inline__)) inline
-#else
-#define inline __forceinline
+#if !defined(__GNUC__)
+  #define __SSE2__
 #endif
 
 #ifdef __GNUC__
-#define __inline __forceinline  // GCC needs to force inlining of intrinsics functions
+  #ifndef __forceinline
+    #define __forceinline __attribute__((__always_inline__)) inline
+  #endif
+#endif
+
+#ifdef __GNUC__
+ #define __inline __forceinline  // GCC needs to force inlining of intrinsics functions
 #endif
 
 #include <mmintrin.h>
 #include <xmmintrin.h>
-#include <emmintrin.h>
-
-#ifdef __GNUC__
-#undef __inline
+#ifdef __SSE2__
+  #include <emmintrin.h>
 #endif
 
 #ifdef __GNUC__
-#define __align8(t,v) t v __attribute__ ((aligned (8)))
-#define __align16(t,v) t v __attribute__ ((aligned (16)))
+ #undef __inline
+#endif
+
+#ifdef __GNUC__
+  #define __align8(t,v) t v __attribute__ ((aligned (8)))
+  #define __align16(t,v) t v __attribute__ ((aligned (16)))
 #else
-#define __align8(t,v) __declspec(align(8)) t v
-#define __align16(t,v) __declspec(align(16)) t v
+  #define __align8(t,v) __declspec(align(8)) t v
+  #define __align16(t,v) __declspec(align(16)) t v
 #endif
 
 #endif
